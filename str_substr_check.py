ab3751d58699bf3bf06eb52412ffde52cc774549
SaikumarPynh <saikumarpynh@gmail.com>
2023-11-30 22:47:56 +0530

    string substring checking

---
+++ str_substr_check.py
@@ -0,0 +1,6 @@
+text = "Python is powerful"
+substring = "power"
+if substring in text:
+    print(f"The substring '{substring}' is present in the text.")
+else:
+    print(f"The substring '{substring}' is not present in the text.")
