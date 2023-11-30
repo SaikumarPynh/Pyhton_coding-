8461980e1db356842f961177465bed80f7b4fbcb
SaikumarPynh <saikumarpynh@gmail.com>
2023-11-30 22:45:15 +0530

    string splitting and joining

---
+++ str_splitting_joining.py
@@ -0,0 +1,5 @@
+text = "Python is a powerful programming language"
+words = text.split()  # Split the string into a list of words
+joined_text = " ".join(words)  # Join the words into a single string with spaces
+print("Split:", words)
+print("Joined:", joined_text)
