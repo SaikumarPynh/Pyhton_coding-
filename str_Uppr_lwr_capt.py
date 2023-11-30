d31b035f4626a09d0a60b3e61e3bf6e77af05bbe
SaikumarPynh <saikumarpynh@gmail.com>
2023-11-30 22:43:16 +0530

    string upper lower and capitalize

---
+++ str_Uppr_lwr_capt.py
@@ -0,0 +1,8 @@
+text = "python programming"
+uppercase_text = text.upper()
+lowercase_text = text.lower()
+capitalized_text = text.capitalize()
+
+print("Uppercase:", uppercase_text)
+print("Lowercase:", lowercase_text)
+print("Capitalized:", capitalized_text)
